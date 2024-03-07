b0e43781f080f21c6456a3109c3a48e3294c3259
Mayara Kesia <109696813+mayarakesia@users.noreply.github.com>
2024-03-07 00:47:43 +0000

    codigos da aula 2 e pratica1

---
+++ aulas/aula01/aula2/constante.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SEXO_MASCULINO 2
+
+int main() {
+  const float PI = 3.14;
+
+  //PI = 3.1415; NUM PODE!!!!
+
+  const int SEXO_MASCULINO = 1;
+  int sexo = SEXO_MASCULINO;
+
+  return 0;
+}
---
+++ aulas/aula01/aula2/variaveis.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int numero;
+  float flutuante;
+  double juros_simples;
+
+  int a;
+  int x1, x2; // as raizes da equacao do 1o grau
+  return 0;
+}
